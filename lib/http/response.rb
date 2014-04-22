7868788b2cf99e5dc07b1a569e2e0b2da30a3f9d
Aleksey V Zapparov <ixti@member.fsf.org>
2014-04-22 04:01:17 +0200

    Extract response status and provide some sugar.
    Example:
    
        res = HTTP.get "http://example.com"
        fail "something wrong" unless res.status.ok?

--- lib/http/response.rb
+++ lib/http/response.rb
@@ -1,70 +1,15 @@
-require 'delegate'
+require 'forwardable'
+
 require 'http/headers'
 require 'http/content_type'
 require 'http/mime_type'
+require 'http/response/status'
 
 module HTTP
   class Response
+    extend Forwardable
+
     include HTTP::Headers::Mixin
-
-    STATUS_CODES = {
-      100 => 'Continue',
-      101 => 'Switching Protocols',
-      102 => 'Processing',
-      200 => 'OK',
-      201 => 'Created',
-      202 => 'Accepted',
-      203 => 'Non-Authoritative Information',
-      204 => 'No Content',
-      205 => 'Reset Content',
-      206 => 'Partial Content',
-      207 => 'Multi-Status',
-      226 => 'IM Used',
-      300 => 'Multiple Choices',
-      301 => 'Moved Permanently',
-      302 => 'Found',
-      303 => 'See Other',
-      304 => 'Not Modified',
-      305 => 'Use Proxy',
-      306 => 'Reserved',
-      307 => 'Temporary Redirect',
-      400 => 'Bad Request',
-      401 => 'Unauthorized',
-      402 => 'Payment Required',
-      403 => 'Forbidden',
-      404 => 'Not Found',
-      405 => 'Method Not Allowed',
-      406 => 'Not Acceptable',
-      407 => 'Proxy Authentication Required',
-      408 => 'Request Timeout',
-      409 => 'Conflict',
-      410 => 'Gone',
-      411 => 'Length Required',
-      412 => 'Precondition Failed',
-      413 => 'Request Entity Too Large',
-      414 => 'Request-URI Too Long',
-      415 => 'Unsupported Media Type',
-      416 => 'Requested Range Not Satisfiable',
-      417 => 'Expectation Failed',
-      418 => "I'm a Teapot",
-      422 => 'Unprocessable Entity',
-      423 => 'Locked',
-      424 => 'Failed Dependency',
-      426 => 'Upgrade Required',
-      500 => 'Internal Server Error',
-      501 => 'Not Implemented',
-      502 => 'Bad Gateway',
-      503 => 'Service Unavailable',
-      504 => 'Gateway Timeout',
-      505 => 'HTTP Version Not Supported',
-      506 => 'Variant Also Negotiates',
-      507 => 'Insufficient Storage',
-      510 => 'Not Extended'
-    }
-    STATUS_CODES.freeze
-
-    SYMBOL_TO_STATUS_CODE = Hash[STATUS_CODES.map { |code, msg| [msg.downcase.gsub(/\s|-/, '_').to_sym, code] }]
-    SYMBOL_TO_STATUS_CODE.freeze
 
     attr_reader :status
     attr_reader :body
@@ -75,18 +20,17 @@ module HTTP
     alias_method :status_code, :status
 
     def initialize(status, version, headers, body, uri = nil) # rubocop:disable ParameterLists
-      @status, @version, @body, @uri = status, version, body, uri
+      @version, @body, @uri = version, body, uri
+
+      @status  = HTTP::Response::Status.new status
       @headers = HTTP::Headers.coerce(headers || {})
     end
 
-    # Obtain the 'Reason-Phrase' for the response
-    def reason
-      STATUS_CODES[@status]
-    end
+    def_delegator :status, :reason
 
     # Returns an Array ala Rack: `[status, headers, body]`
     def to_a
-      [status, headers.to_h, body.to_s]
+      [status.to_i, headers.to_h, body.to_s]
     end
 
     # Return the response body as a string
@@ -131,7 +75,7 @@ module HTTP
 
     # Inspect a response
     def inspect
-      "#<#{self.class}/#{@version} #{status} #{reason} headers=#{headers.inspect}>"
+      "#<#{self.class}/#{@version} #{status.inspect} #{headers.inspect}>"
     end
   end
 end
